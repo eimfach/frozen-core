e197a844e3a9754add4fa66ffc9af642e51d8a2a
eimfach <eimfach@me.com>
2015-04-27 00:27:46 +0200

    refactoring: only one param, removed deadend, cleaned up, simplified

--- capsule.js
+++ capsule.js
@@ -1,153 +1,107 @@
-(function(module){
-    'use strict';
+'use strict';
 
-    var strict = false;
-    var Capsule = {
+var strict = false;
+var Capsule = {
 
-        /**
-         * @param state Spec for Object state, mutable properties
-         * @param core Spec for immutable properties (Provide just one Object for pureness)
-         * @param deadEnd Spec for non inheritable properties
-         */
-        extend: function(state, core, deadEnd){
-            //param check
-            if(typeof state !== "object") state = {};
-            if(core === undefined) core = state;
-            if(typeof core !== "object") core = {};
-            if(typeof state !== "object") state = {};
-            if(typeof deadEnd !== "object") deadEnd = {};
+    /**
+     * @param options Spec for Object
+     * options.state Spec for mutable properties
+     * options.core Spec for immutable properties (Provide just one Object for pureness)
+     * options.deadEnd Spec for non inheritable properties
+     */
+    extend: function(options){
+        if(typeof options !== 'object') options = {};
+        var core = options.core || {},
+            state = options.state || {};
 
-            // to not insert deadend properties into the inherited closures
-            var parentObject = {};
-            var coreMergeObject = {};
+        var parentObject = {};
+        var coreMergeObject = {};
 
-            // add a extension method to each object within its core, which others can inherit from
-            core.extend = function(stateExtension, coreExtension, deadEnd){
+        // add a extension method to each object within its core, which others can inherit from
+        core.extend = function(options){
+            if(typeof options !== 'object') options = {};
+            options.core = extendObject(options.core || {}, core, true );
+            options.state = extendObject(options.state || {}, state );
+            options.core.parent = parentObject;
 
-                //param check
-                if(typeof stateExtension !== "object") stateExtension = {};
-                if(typeof coreExtension !== "object") coreExtension = stateExtension;
-                if(state === core) state = {}; //if a parent state was never given parent state equals core - prevent that
-                var mergeObject = {};
+            return Capsule.extend(options);
+        };
+        core.bubble = function(call){
+            if(typeof parentObject[call] === "function"){
+                if(typeof parentObject.parent === "object"){
 
-                //extend / overwrite the parent state
-                state = extendObject(stateExtension, state);
-                core = extendObject(coreExtension, core, true);
-
-                core.parent = parentObject;
-
-                return Capsule.extend(state, core, deadEnd);
-            };
-            core.bubble = function(call){
-                if(typeof parentObject[call] === "function"){
-                    if(typeof parentObject.parent === "object"){
-
-                        if(parentObject[call] !== parentObject.parent[call]){
-                            parentObject[call]();
-
-                        } // else -> the function is a reference to its parents one TODO: evaluate
-                        parentObject.parent.bubble(call);
-                    } else {
-                        parentObject[call](); // parent is the forebear, bubbling end
-                    }
-                } // bubble interrupted //TODO: evaluate throwing error on strict mode //TODO: evaluate this possibility in general
-
-                return true;
-            };
-
-
-            /**STATE**/
-                //extend the new object with the state object
-            parentObject = extendObject(state, parentObject);
-
-            //param check
-            var stateDeadend = {};
-            if(deadEnd.constructor === Array &&
-                deadEnd.length >= 2 &&
-                typeof deadEnd[0] === "object"){
-
-                stateDeadend = deadEnd[0];
-            }
-            //extend the new object with the state deadend properties
-            parentObject = extendObject(stateDeadend, parentObject);
-
-            /**CORE**/
-
-            //param check
-            var coreDeadend = {};
-            if(typeof deadEnd === "object"){
-
-                if(deadEnd.constructor === Array){
-                    if(deadEnd.length === 1 && typeof deadEnd[0] === "object"){
-
-                        coreDeadend = deadEnd[0];
-
-                    } else if(deadEnd.length >= 2 && typeof deadEnd[1] === "object"){
-                        coreDeadend = deadEnd[1];
-                    }
+                    parentObject[call]();
+                    parentObject.parent.bubble(call);
                 } else {
-                    coreDeadend = deadEnd;
+                    parentObject[call](); // parent is the forebear, bubbling end
                 }
+            } // bubble interrupted //TODO: evaluate throwing error on strict mode //TODO: evaluate this possibility in general
 
-            }
-            // extend the core merge object with the deadend core properties from the deadend object
-            coreMergeObject = extendObject(coreDeadend, coreMergeObject);
-
-            //merge core objects
-            coreMergeObject = extendObject(core, coreMergeObject);
-
-            // freeze the core
-            var frozenCore = Object.freeze(coreMergeObject);
-
-            //now extend the new object with the core properties and their descriptors
-            Object.keys(frozenCore).forEach(function(property){
-                parentObject[property] = frozenCore[property];
-                Object.defineProperty(parentObject, property, Object.getOwnPropertyDescriptor(frozenCore, property) );
-
-            });
-
-            // return the new object sealed
-            return Object.seal(parentObject);
-        },
-
-        enableStrict: function(){
-            strict = true;
-        }
+            return true;
+        };
 
 
-    };
+        /**STATE**/
+            //extend the new object with the state object
+        parentObject = extendObject(parentObject, state);
 
-    /* pure */
-    var extendObject = function(parent, child, typesafe){
+        /**CORE**/
+
+        //merge core objects
+        coreMergeObject = extendObject(coreMergeObject, core);
+
+        // freeze the core
+        var frozenCore = Object.freeze(coreMergeObject);
+
+        //now extend the new object with the core properties and their descriptors
+        Object.keys(frozenCore).forEach(function(property){
+            parentObject[property] = frozenCore[property];
+            Object.defineProperty(parentObject, property, Object.getOwnPropertyDescriptor(frozenCore, property) );
+
+        });
+
+        // return the new object sealed
+        return Object.seal(parentObject);
+    },
+
+    enableStrict: function(){
+        strict = true;
+    }
+
+
+};
+
+/* pure */
+var extendObject = function(child, parent, typesafe){
 
 //TODO: property descriptor inheritance
 
-        Object.keys(parent).forEach(function(property){
+    var result = {};
 
-            //TODO: flip parent/child logic
-            if(typesafe === true){
+    var merge = function(object, safe){
+
+        Object.keys(object).forEach(function(property){
+
+            if(safe === true){
                 // has the core this property already?
-                if(typeof child[property] === "undefined"){
-                    child[property] = parent[property];
+                if(typeof result[property] === "undefined" || typeof result[property] === typeof object[property]){
+                    result[property] = object[property];
 
-                    //property is already in parent
-                } else if(typeof parent[property] === typeof child[property]) {
-
-                    //overwrite on same type
-                    child[property] = parent[property];
-
-                } else if(strict === true){
-                    throw Capsule.extend({type: 'TypeError', 'message':'Mismatch of type from property'});
+                }  else if(strict === true){
+                    throw new TypeError();
                 }
             } else {
-                child[property] = parent[property];
+                result[property] = object[property];
             }
         });
-        return child;
     };
+    merge(parent);
+    merge(child, typesafe);
 
-    module.exports = Capsule;
+    return result;
+};
 
-})(module);
+module.exports = Capsule;
+
 
 
--- test/capsule.js
+++ test/capsule.js
@@ -33,242 +33,227 @@ describe('Capsule', function(){
     });
     it('should merge two objects into a new one and provides an extend function on the new object.', function(){
 
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        object.should.have.property('root').that.equals("mutable");
-        object.should.have.property('core').that.equals("immutable");
+        var object = Capsule.extend({
+            state: {
+                model: 'mutable',
+                face: 'immutable'
+            }
+        });
+        object.should.have.property('model').that.equals("mutable");
+        object.should.have.property('face').that.equals("immutable");
     });
     it('should provide an extend function to the merged object', function(){
 
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
+        var object = Capsule.extend();
         object.should.have.property('extend').that.is.a("function");
 
     });
-    it('should be possible to modify the property values given by the first parameter on construction.', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
+    it('should be possible to modify the property values given by the state object.', function(){
+        var object = Capsule.extend({
+            state: {
+                model: 'mutable',
+                face: 'immutable'
+            }
+        });
 
-        object.root = "modified";
-        object.root.should.equal("modified");
+        object.model = "modified";
+        object.model.should.equal("modified");
     });
     it('should not be possible to remove or add properties to the merged object.', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
+        var object = Capsule.extend({
+            state: {
+                model: 'mutable',
+                face: 'immutable'
+            }
+        });
 
-        delete object.root;
-        object.root.should.exist;
-        object.root.should.be.a("string");
+        delete object.model;
+        object.model.should.exist;
+        object.model.should.be.a("string");
 
         object.newProperty = 1;
         (1).should.not.equal(object.newProperty);
 
 
     });
-    it('should not be possible to modify the property values given my the second parameter on construction (frozen core).', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
+    it('should not be possible to modify the property values given by the core object (frozen core).', function(){
+        var object = Capsule.extend({
+            core: {
+                id: 'immutable'
+            }
+        });
 
-        object.core = "modified";
-        object.core.should.equal("immutable");
+        object.id = "modified";
+        object.id.should.equal("immutable");
     });
     it('should be possible to extend an merged object with new properties', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var newObject = object.extend({rootExtension: "mutable"}, {coreExtension: "immutable"});
-        newObject.should.have.property('root').that.equals("mutable");
-        newObject.should.have.property('core').that.equals("immutable");
-        newObject.should.have.property('rootExtension').that.equals("mutable");
-        newObject.should.have.property('coreExtension').that.equals("immutable");
-        newObject.should.have.property('extend').that.is.a("function");
+        var object = Capsule.extend({
+            state: {
+                model: 'mutable',
+                face: 'mutable'
+            }
+        });
+
+        var newObject = object.extend({
+            state:{
+                session: "mutable",
+                lifetime: "mutable"
+            }
+        });
+        newObject.should.have.property('model').that.equals("mutable");
+        newObject.should.have.property('face').that.equals("mutable");
+        newObject.should.have.property('session').that.equals("mutable");
+        newObject.should.have.property('lifetime').that.equals("mutable");
     });
     it('creates a child object which should have a reference to its creator', function(){
 
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var child = object.extend({root: "mutable"});
+        var object = Capsule.extend({model: "mutable"}, {face: "immutable"});
+        var child = object.extend({model: "mutable"});
         child.should.have.property('parent').that.deep.equals(object);
 
 
     });
-    it('should overwrite existing state property values with equal ones (if given) on extension', function(){
+    it('should overwrite existing state property values', function(){
 
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var child = object.extend({root: "modified"}, {core: "immutable"});
-        child.should.have.property('root').that.equals("modified");
+        var object = Capsule.extend({
+            state:{
+                model: "mutable",
+                face: "mutable"
+            }
+        });
+        var child = object.extend({
+            state:{
+                model: "modified"
+            }
+        });
+        child.should.have.property('model').that.equals("modified");
+        child.should.have.property('face').that.equals("mutable");
     });
-    it('should overwrite existing core properties with equal ones (if given) on extension', function(){
+    it('should overwrite existing core properties on extension', function(){
 
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var child = object.extend({root: "modified"}, {core: "modified"});
-        child.should.have.property('core').that.equals("modified");
+        var object = Capsule.extend({
+            core:{
+                id: "immutable",
+                create: function(){}
+            }
+        });
+
+        var child = object.extend({
+            core:{
+                id: "modified"
+            }
+        });
+        child.should.have.property('id').that.equals("modified");
+        child.should.have.property('create').that.is.a("function");
     });
-    it('should prevent overwriting core properties with wrong datatypes on extension', function(){
+    it('should prevent overwriting core properties with wrong types on extension', function(){
 
-        var object = Capsule.extend({nooverwrite: function(){}});
-        var child = object.extend({nooverwrite: "modified"});
-        child.nooverwrite.should.be.a("function");
+        var object = Capsule.extend({
+            core:{
+                id: "immutable"
+            }
+        });
+
+        var child = object.extend({
+            core:{
+                id: function(){}
+            }
+        });
+        child.should.have.property('id').that.is.a("string");
     });
 
 
     it('prevent overwriting its own core properties ', function(){
         //this is no spec for the core properties !
-        var object = Capsule.extend({core: "immutable"});
-        var child = object.extend({extend: function(){return "modified"}, parent: {}, bubble: function(){return "modified"}});
+        var object = Capsule.extend({
+            core:{
+                id: "immutable"
+            }
+        });
+        var child = object.extend({
+            core:{
+                extend: function(){return "modified"},
+                parent: {},
+                bubble: function(){return "modified"}
+            }}
+        );
+
         var child2 = child.extend();
+
         child2.should.be.a('object').that.not.equals("modified");
         child.parent.should.not.deep.equal({});
         var result = child.bubble();
         result.should.not.equal("modified");
     });
-    it('should be possible to modify the property values on the child object (created by extension) given by the first parameter on construction and the first parameter on extension', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var newObject = object.extend({rootExtension: "mutable"}, {coreExtension: "immutable"});
+    it('should be possible to modify the property values from child extension state object', function(){
+        var object = Capsule.extend({
+            state:{
+                model: "mutable"
+            }
+        });
 
-        newObject.rootExtension = "modified";
-        newObject.rootExtension.should.equal("modified");
+        var child = object.extend({
+            state:{
+                session: function(){}
+            }
+        });
 
-        newObject.root = "modified";
-        newObject.root.should.equal("modified");
+        child.session = "modified";
+        child.session.should.equal("modified");
 
-    });
-    it('should not be possible to remove or add properties to the child object (created by extension).', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var newObject = object.extend({rootExtension: "mutable"}, {coreExtension: "immutable"});
-
-        delete object.rootExtension;
-        newObject.rootExtension.should.exist;
-        newObject.rootExtension.should.be.a("string");
-
-        delete object.root;
-        newObject.root.should.exist;
-        newObject.root.should.be.a("string");
-
-        newObject.newProperty = 1;
-        (1).should.not.equal(newObject.newProperty);
-
+        child.model = "modified";
+        child.model.should.equal("modified");
 
     });
-    it('should not be possible to modify the property values given my the second parameter on construction and the second parameter on extension (frozen core).', function(){
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"});
-        var newObject = object.extend({rootExtension: "mutable"}, {coreExtension: "immutable"});
-
-        object.core = "modified";
-        object.core.should.equal("immutable");
-
-        newObject.coreExtension = "modified";
-        newObject.coreExtension.should.equal("immutable");
-    });
-
-    it('should return a complete frozen object if only one parameter is given', function(){
-
-        var object = Capsule.extend({root: "immutable"});
-
-        delete object.root;
-        object.root.should.exist;
-        object.root.should.be.a("string");
-
-        object.newProperty = 1;
-        (1).should.not.equal(object.newProperty);
-
-        object.root = "modified";
-        object.root.should.equal("immutable");
-
-        var child = object.extend({core: "immutable"});
-        child.core = "modified";
-        child.core.should.equal("immutable");
-
-        Object.isFrozen(object).should.be.true;
-        Object.isFrozen(child).should.be.true;
-    });
-    it('should accept an (dead-end) array or object as parameter or a Capsule which will not be inherited on extension', function(){
-
-        //param check
-        var obj = Capsule.extend({root: "mutable"}, {core: "immutable"}, [{rootDeadend: "mutable"}, {coreDeadend: "immutable"}]);
-        obj.should.have.property('root').that.equals("mutable");
-        obj.should.have.property('core').that.equals("immutable");
-        obj.should.have.property('rootDeadend').that.equals("mutable");
-        obj.should.have.property('coreDeadend').that.equals("immutable");
-
-        var obj = Capsule.extend({root: "mutable"}, {core: "immutable"}, [{coreDeadend: "immutable"}]);
-        obj.should.have.property('root').that.equals("mutable");
-        obj.should.have.property('core').that.equals("immutable");
-        obj.should.have.property('coreDeadend').that.equals("immutable");
-
-        var obj = Capsule.extend({root: "mutable"}, {core: "immutable"}, {coreDeadend: "immutable"});
-        obj.should.have.property('root').that.equals("mutable");
-        obj.should.have.property('core').that.equals("immutable");
-        obj.should.have.property('coreDeadend').that.equals("immutable");
-
-
-        //inheritation check
-        var object = Capsule.extend({root: "mutable"}, {core: "immutable"}, [{rootDeadend: "mutable", coreDeadend: "immutable"}]);
-        var child1 = object.extend({rootExtension: "mutable"}, {coreExtension: "immutable"}, [{newDeadendRoot: "mutable"},{newDeadendCore: "immutable"}]);
-        child1.should.have.property('root').that.equals("mutable");
-        child1.should.have.property('core').that.equals("immutable");
-        child1.should.have.property('rootExtension').that.equals("mutable");
-        child1.should.have.property('coreExtension').that.equals("immutable");
-        child1.should.have.property('newDeadendRoot').that.equals("mutable");
-        child1.should.have.property('newDeadendCore').that.equals("immutable");
-        child1.should.not.have.property('rootDeadend');
-        child1.should.not.have.property('coreDeadend');
-
-
-        var object1 = Capsule.extend({root: "mutable"}, {core: "immutable"}, {coreDeadend: "immutable"});
-        var child2 = object1.extend({rootExtension: "mutable"}, {coreExtension: "immutable"}, {newDeadendCore: "immutable"});
-        child2.should.have.property('root').that.equals("mutable");
-        child2.should.have.property('core').that.equals("immutable");
-        child2.should.have.property('rootExtension').that.equals("mutable");
-        child2.should.have.property('coreExtension').that.equals("immutable");
-        child2.should.have.property('newDeadendCore').that.equals("immutable");
-        child2.should.not.have.property('rootDeadend');
-        child2.should.not.have.property('coreDeadend');
-
-        var object1 = Capsule.extend({root: "mutable"}, {core: "immutable"}, [{coreDeadend: "immutable"}]);
-        var child2 = object1.extend({rootExtension: "mutable"}, {coreExtension: "immutable"}, [{newDeadendCore: "immutable"}]);
-        child2.should.have.property('root').that.equals("mutable");
-        child2.should.have.property('core').that.equals("immutable");
-        child2.should.have.property('rootExtension').that.equals("mutable");
-        child2.should.have.property('coreExtension').that.equals("immutable");
-        child2.should.have.property('newDeadendCore').that.equals("immutable");
-        child2.should.not.have.property('rootDeadend');
-        child2.should.not.have.property('coreDeadend');
-
-
-    });
-
     it('should have a function "bubble" which executes all parent functions and its own', function(){
 
         var root = Capsule.extend({
-            title: "root",
-            render: function(){
-                root.title = "modified"
+            core: {
+                render: function(){
+                    this.title = "modified"
+                }
+            },
+            state: {
+                title: "root"
             }
-        }, {
 
         });
+
         var master = root.extend({
-            title: "master"
-        }, {
-
-        });
-        var parent = master.extend({
-            title: "parent",
-            render: function(){
-                parent.title = "modified";
+            state: {
+                title: 'master'
             }
-        }, {
-
         });
 
-        var child = parent.extend({
-            title: "child",
-            render: function(){
-                child.title = "modified";
+        var slave1 = master.extend({
+            core: {
+                render: function(){
+                    this.title = 'slave1 modified';
+                }
+            },
+            state: {
+                title: 'slave1'
             }
-        }, {
         });
 
-        child.should.have.property('bubble').that.is.a('function');
+        var slave2 = master.extend({
+            core: {
+                render: function(){
+                    this.title = 'slave2 modified';
+                }
+            },
+            state: {
+                title: 'slave2'
+            }
+        });
 
-        child.bubble('render');
+        slave2.should.have.property('bubble').that.is.a('function');
 
-        child.title.should.equal("modified");
-        parent.title.should.equal("modified");
-        master.title.should.equal("master");
+        slave2.bubble('render');
+
+        slave2.title.should.equal("slave2 modified");
+        slave1.title.should.equal("slave1");
+        master.title.should.equal("modified");
         root.title.should.equal("modified");
 
     });
@@ -278,16 +263,23 @@ describe('Capsule', function(){
         it('should throw a typeerror on try to overwrite with wrong datatype', function(){
 
             Capsule.enableStrict();
-            var object = Capsule.extend({nooverwrite: function(){}});
+            var object = Capsule.extend({
+                core: {
+                    id: 'immutable'
+                }
+            });
             var errorThrown = false;
 
             try{
-                var child = object.extend({nooverwrite: "modified"});
+                var child = object.extend({
+                    core: {
+                        id: function(){}
+                    }
+                });
 
             } catch(e){
                 errorThrown = true;
-                e.should.have.property('type').that.equals('TypeError');
-                e.should.have.property('message').that.is.not.empty;
+                e.should.be.an.instanceof(TypeError);
             }
             errorThrown.should.be.true;
         });
